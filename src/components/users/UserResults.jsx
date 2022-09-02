cec3cb87958dbaf122d650b077e3666b74b1dcdb
jquick5 <jay.quick79@gmail.com>
2022-09-02 15:28:07 -0700

    Ui building for main search

---
+++ src/components/users/UserResults.jsx
@@ -0,0 +1,22 @@
+import { useContext } from 'react';
+import Spinner from '../layout/Spinner';
+import UserItem from './UserItem';
+import GithubContext from '../../context/github/GithubContext';
+
+const UserResults = () => {
+	const { users, loading } = useContext(GithubContext);
+
+	if (!loading) {
+		return (
+			<div className='grid grid-cols-1 gap-8 xl:grid-cols-4 lg:grid-cols-3 md:grid-cols-2'>
+				{users.map((u) => (
+					<UserItem key={u.id} user={u} />
+				))}
+			</div>
+		);
+	} else {
+		return <Spinner />;
+	}
+};
+
+export default UserResults;
